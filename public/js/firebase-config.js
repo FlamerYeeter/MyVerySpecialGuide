4754d525b19eb0b539bab2a94fcd08804282189e
Thomas Adrian M. Naguit <tnaguit.a12241643@umak.edu.ph>
2025-09-21 16:28:49 +0800

    Create firebase-config.js

---
+++ public/js/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb2wiXLmVeOJS6T25tXk8sgbgp4zJvqRM",
+  authDomain: "myveryspecialguide.firebaseapp.com",
+  projectId: "myveryspecialguide",
+  storageBucket: "myveryspecialguide.firebasestorage.app",
+  messagingSenderId: "1092348597749",
+  appId: "1:1092348597749:web:2e662681864dd53c9cbcbc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
