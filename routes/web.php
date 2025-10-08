536372302f8c1835ce4f1eb80e357c27d7a41be6
Jao Nicholas <jbenedicto.k12043659@umak.edu.ph>
2025-10-08 21:47:03 +0800

    added includes for header in each feature

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,11 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
+/*
 Route::get('/', function () {
     return view('job_application_2');
 })->name('job_application_2');
+*/
 
 Route::get('/register', function () {
     return view('ds_register_1');
